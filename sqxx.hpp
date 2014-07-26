a1c25ed69707ff000d89ebd5c543f41a862a867b
Stephan Hohe <sth.dev@tejp.de>
2014-07-26 06:06:15 +0200

    Removed unnecessary includes

--- sqxx.hpp
+++ sqxx.hpp
@@ -4,13 +4,6 @@
 #if !defined(SQXX_SQXX_HPP_INCLUDED)
 #define SQXX_SQXX_HPP_INCLUDED
 
-#include "datatypes.hpp"
-#include <stdexcept>
-#include <vector>
-#include <memory>
-#include <utility>
-
-#include "error.hpp"
 #include "connection.hpp"
 #include "statement.hpp"
 
